a3837a5aed503dc436338496dd7e961c776e69da
Alejandro Vasquez <alejandro.vasquez@upr.edu>
2020-03-21 20:10:53 -0400

    feat(routes): added routes for home and about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,17 @@ import {VisualizeComponent} from './modules/visualize/visualize.component';
 import {DocumentsComponent} from './modules/documents/documents.component';
 import {AppComponent} from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './modules/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
   {
     path: 'collab-request',
