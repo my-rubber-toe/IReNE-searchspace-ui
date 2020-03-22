ca3e2111a39f38ad270146fc0d91244e49efd4ff
Alejandro Vasquez <alejandro.vasquez@upr.edu>
2020-03-21 20:15:06 -0400

    feat(app component): added toolbar and side navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CollabRequestComponent } from './modules/collab-request/collab-request.component';
 import { DocumentsComponent } from './modules/documents/documents.component';
 import { VisualizeComponent } from './modules/visualize/visualize.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './modules/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     CollabRequestComponent,
     DocumentsComponent,
-    VisualizeComponent
+    VisualizeComponent,
+    AboutComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
