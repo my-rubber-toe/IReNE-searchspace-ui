131b8b1c05640087aed492ef449a3ee5e579881a
Robert G <roberto.guzman3@upr.edu>
2020-04-01 22:21:34 -0400

    playing with smooth scroll

--- src/app/modules/documents/documents.component.ts
+++ src/app/modules/documents/documents.component.ts
@@ -23,6 +23,15 @@ export class DocumentsComponent implements OnInit {
   constructor(private docservice:SearchSpaceService) { }
 
   ngOnInit(): void {
+    // Smooth scroll up
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+          window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+      }, 16);
     this.docservice.getDocuments()
   }
 }
