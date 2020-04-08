b773fbef7330bcc2f02c0369e7c98411a4c1240b
Alejandro Vasquez <alejandro.vasquez@upr.edu>
2020-04-07 21:16:21 -0400

    refactor(documents): refactor the code of filtering documents
    clean the code and make it more general

--- src/app/modules/documents/documents-table/documents-table.component.ts
+++ src/app/modules/documents/documents-table/documents-table.component.ts
@@ -23,8 +23,7 @@ export class DocumentsTableComponent implements OnInit {
     'publication_date', 'incident_date', 'modification_date', 'infrastructure_type',
     'damage_type', 'language', 'tag', 'actions'];
   filterSelection: Map<string, any> = new Map<string, any>([
-   // ['location', ''],
-    ['creators', ''],
+    ['creator', ''],
     ['infrastructure_type', ''],
     ['damage_type', ''],
     ['language', ''],
@@ -33,17 +32,24 @@ export class DocumentsTableComponent implements OnInit {
     ['publication_date', '']
   ]);
 
+  constructor(
+    private documentService: SearchSpaceService,
+    private router: Router
+  ) {
+  }
+
   applyFilter() {
     const filteringDataSource = new MatTableDataSource<DocumentMetadata>();
-    // this.filterByLocations(this.filterSelection.get('location'), filteringDataSource);
-    this.filterByLanguage(this.filterSelection.get('language'), filteringDataSource);
-    this.filterBySelection(this.filterSelection.get('tag'), filteringDataSource, 'tag');
-    this.filterBySelection(this.filterSelection.get('damage_type'), filteringDataSource, 'damage_type');
-    this.filterBySelection(this.filterSelection.get('infrastructure_type'), filteringDataSource, 'infrastructure_type');
-    this.filterBySelection(this.filterSelection.get('publication_date'), filteringDataSource, 'publication_date');
-    this.filterBySelection(this.filterSelection.get('incident_date'), filteringDataSource, 'incident_date');
-    this.filterBySelection(this.filterSelection.get('creators'), filteringDataSource, 'creators');
+    filteringDataSource.data = this.tempDataSource.data;
+    const ite = this.filterSelection.keys();
+    for (const key of ite) {
+      this.filterBySelection(this.filterSelection.get(key), filteringDataSource, key);
+    }
     this.dataSource = filteringDataSource;
+    if (this.tempEvent) {
+      this.searchFilter(this.tempEvent);
+    }
+    this.paginateSort(this.dataSource);
   }
 
   searchFilter(event: Event) {
@@ -66,12 +72,6 @@ export class DocumentsTableComponent implements OnInit {
     }
   }
 
-  constructor(
-    private documentService: SearchSpaceService,
-    private router: Router
-    ) {
-  }
-
   ngOnInit(): void {
     this.documentService.getDocuments().add(() => {
       this.dataSource =  new MatTableDataSource<DocumentMetadata>(this.documentService.documents);
@@ -92,108 +92,25 @@ export class DocumentsTableComponent implements OnInit {
       this.tempDataSource = this.dataSource;
     });
   }
+
   filterBySelection(filter: string[], filteringDataSource: MatTableDataSource<DocumentMetadata>, selection: string) {
     const tempFilterData = new MatTableDataSource<DocumentMetadata>();
-    if (filter.length !== 0 || selection === 'language') {
-      switch (selection) {
-        case 'damage_type':
-          filteringDataSource.data.forEach(e => {
-            for (const value of filter) {
-              if (e.damage_type.includes(value)) {
-                tempFilterData.data.push(e);
+    if (filter.length !== 0) {
+          const ite = filteringDataSource.data.values();
+          for (const value of ite) {
+            for (const x of filter) {
+              if (value[selection].includes(x)) {
+                tempFilterData.data.push(value);
                 break;
               }
             }
-          });
-          break;
-        case 'infrastructure_type':
-          filteringDataSource.data.forEach(e => {
-            for (const value of filter) {
-              if (e.infrastructure_type.includes(value)) {
-                tempFilterData.data.push(e);
-                break;
-              }
-            }
-          });
-          break;
-        case 'tag':
-          filteringDataSource.data.forEach(e => {
-            for (const value of filter) {
-              if (e.tag.includes(value)) {
-                tempFilterData.data.push(e);
-                break;
-              }
-            }
-          });
-          break;
-        case 'publication_date':
-          filteringDataSource.data.forEach(e => {
-              if (e.publication_date.includes(filter.toString())) {
-                tempFilterData.data.push(e);
-              }
-          });
-          break;
-        case 'incident_date':
-          filteringDataSource.data.forEach(e => {
-            if (e.incident_date.includes(filter.toString())) {
-              tempFilterData.data.push(e);
-            }
-          });
-          break;
-        case 'creators':
-          filteringDataSource.data.forEach(e => {
-            for (const value of filter) {
-              if (e.creator.includes(value)) {
-                tempFilterData.data.push(e);
-                break;
-              }
-            }
-          });
-          break;
+          }
       }
+    if (tempFilterData.data.length !== 0) {
       filteringDataSource.data = tempFilterData.data;
     }
-  }
-
-  filterByLanguage(language: string[], filteringDataSource: MatTableDataSource<DocumentMetadata>) {
-    if (language.length !== 0) {
-      this.tempDataSource.data.forEach(e => {
-        language.forEach(s => {
-          if (e.language === s) {
-            filteringDataSource.data.push(e);
-          }
-        });
-      });
-    } else {
-      filteringDataSource.data = this.tempDataSource.data;
     }
-  }
-
-  // locationEvent(location: string) {
-  //   if (location.length === 0) {
-  //     this.dataSource = this.tempDataSource;
-  //   }
-  //   this.filterSelection.set('location', location);
-  // }
-
-/** filterByLocations(location: string[], filteringDataSource: MatTableDataSource<DocumentMetadata>) {
-      if (location.length !== 0) {
-      this.tempDataSource.data.forEach(e => {
-        location.forEach(s => {
-          if (e.location === s) {
-            filteringDataSource.data.push(e);
-          }
-        });
-      });
-    } else {
-      filteringDataSource.data = this.tempDataSource.data;
-    }
-  } **/
-
   selectionEvent(selection: any, type: string) {
-    if (selection.length === 0) {
-      this.dataSource = this.tempDataSource;
-    }
     this.filterSelection.set(type, selection);
   }
 
