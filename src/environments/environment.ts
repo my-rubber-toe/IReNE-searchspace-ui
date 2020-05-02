bb2c67f798e54ace850f4615c9cd24f1c749ce43
Jainel M. Torres Santos <jainel.torres@upr.edu>
2020-05-02 19:51:36 -0400

    fix(xy): ui fixes & merged hashmap filling locig from integrationRefactor
    set dynamically height of graph depending of num of rows, changed size of all text in graph and used logic of filling the hashmap from integrationrefactor branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   GOOGLE_MAPS_API_KEY: 'AIzaSyAZEkjgNHbvCFQ4ohopyKSg3-zbfHx4pSk',
-  serverUrl: 'https://test.irene.uprm.edu/searchspace-server',
+  // serverUrl: 'https://test.irene.uprm.edu/searchspace-server',
+  serverUrl: 'http://localhost:5000'
 };
 
 /*
